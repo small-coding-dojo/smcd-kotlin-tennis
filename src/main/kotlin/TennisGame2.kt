f94cde72b27445cd15b2981eb6e9db53d36de679
Niklas Rueber <nrueber@igus.net>
2022-10-12 17:29:16 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/kotlin/TennisGame2.kt
+++ src/main/kotlin/TennisGame2.kt
@@ -7,48 +7,32 @@ class TennisGame2(private val player1Name: String, private val player2Name: Stri
 
     override fun getScore(): String {
         var score = ""
-        if (P1point == P2point && P1point < 4) {
-            score = pointsAsText(score, P1point)
-            score += "-All"
-        }
-        if (P1point == P2point && P1point >= 3)
-            score = "Deuce"
-
-        if (P1point > 0 && P2point == 0) {
+        if (P1point == P2point) {
+            if (P1point == P2point && P1point < 4) {
+                score = pointsAsText(score, P1point)
+                score += "-All"
+            }
+            if (P1point == P2point && P1point >= 3)
+                score = "Deuce"
+        } else {
             P1res = pointsAsText(score, P1point)
             P2res = pointsAsText(score, P2point)
             score = "$P1res-$P2res"
-        }
-        if (P2point > 0 && P1point == 0) {
-            P1res = pointsAsText(score, P1point)
-            P2res = pointsAsText(score, P2point)
-            score = "$P1res-$P2res"
-        }
-        if (P1point > P2point && P1point < 4) {
-            P1res = pointsAsText(score, P1point)
-            P2res = pointsAsText(score, P2point)
-            score = "$P1res-$P2res"
-        }
-        if (P2point > P1point && P2point < 4) {
-            P1res = pointsAsText(score, P1point)
-            P2res = pointsAsText(score, P2point)
-            score = "$P1res-$P2res"
-        }
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1"
-        }
 
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2"
-        }
+            if (P1point > P2point && P2point >= 3) {
+                score = "Advantage player1"
+            }
+            if (P2point > P1point && P1point >= 3) {
+                score = "Advantage player2"
+            }
 
-        if (P1point >= 4 && P2point >= 0 && P1point - P2point >= 2) {
-            score = "Win for player1"
+            if (P1point >= 4 && P2point >= 0 && P1point - P2point >= 2) {
+                score = "Win for player1"
+            }
+            if (P2point >= 4 && P1point >= 0 && P2point - P1point >= 2) {
+                score = "Win for player2"
+            }
         }
-        if (P2point >= 4 && P1point >= 0 && P2point - P1point >= 2) {
-            score = "Win for player2"
-        }
-
         return score
     }
 
